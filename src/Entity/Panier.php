c33453524e43dd624413dc8bfca9765a58fc1c97
Cassandre <cassandre.gaudinat@gmail.com>
2020-04-17 14:46:26 +0200

    projet full avec entity et crud

---
+++ src/Entity/Panier.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\PanierRepository")
+ */
+class Panier
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\User", inversedBy="paniers")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $Utilisateur;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $Date_achat;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $Etat;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\ContenuPanier", inversedBy="Panier")
+     */
+    private $contenuPanier;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUtilisateur(): ?User
+    {
+        return $this->Utilisateur;
+    }
+
+    public function setUtilisateur(?User $Utilisateur): self
+    {
+        $this->Utilisateur = $Utilisateur;
+
+        return $this;
+    }
+
+    public function getDateAchat(): ?\DateTimeInterface
+    {
+        return $this->Date_achat;
+    }
+
+    public function setDateAchat(\DateTimeInterface $Date_achat): self
+    {
+        $this->Date_achat = $Date_achat;
+
+        return $this;
+    }
+
+    public function getEtat(): ?bool
+    {
+        return $this->Etat;
+    }
+
+    public function setEtat(bool $Etat): self
+    {
+        $this->Etat = $Etat;
+
+        return $this;
+    }
+
+    public function getContenuPanier(): ?ContenuPanier
+    {
+        return $this->contenuPanier;
+    }
+
+    public function setContenuPanier(?ContenuPanier $contenuPanier): self
+    {
+        $this->contenuPanier = $contenuPanier;
+
+        return $this;
+    }
+}
