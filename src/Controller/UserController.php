b3cf0a4cb6267187f180533c9911fbf14887cdd9
Cassandre <cassandre.gaudinat@gmail.com>
2020-04-19 18:34:10 +0200

    Page profil

---
+++ src/Controller/UserController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\RegistrationFormType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
+use App\Security\UserAuthenticator;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class UserController extends AbstractController
+{
+    /**
+     * @Route("/profil", name="profil")
+     */
+    public function index(Request $request, UserInterface $user)
+    {
+        return $this->render('user/index.html.twig', [
+            'user' => $user,
+        ]);
+    }
+}
