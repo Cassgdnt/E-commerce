62460b4471118124a9908a782f27f81e2bbc6c2e
Cassandre <cassandre.gaudinat@gmail.com>
2020-04-26 19:51:25 +0200

    edit user

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\RegistrationFormType;
+use App\Form\UserType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Request;
@@ -22,9 +23,28 @@ class UserController extends AbstractController
      * @Route("/profil", name="profil")
      */
     public function index(Request $request, UserInterface $user)
-    {
+    {   
         return $this->render('user/index.html.twig', [
             'user' => $user,
+        ]);
+    }
+    /**
+     * @Route("/profil/edit/{id}", name="editProfil")
+     */
+    public function edit(Request $request, User $user){
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('profil');
+        }
+
+        return $this->render('user/edit.html.twig', [
+            'user' => $user,
+            'form_user' => $form->createView(),
         ]);
     }
 }
